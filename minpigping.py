236abbeb09f5346d748cdd65873f34db615d3685
Matheus Freire <santosmatheus99@hotmail.com>
2022-10-25 19:42:08 -0300

    mundando retorno endpoint home

--- minpigping.py
+++ minpigping.py
@@ -1,12 +1,21 @@
+from unicodedata import name
 from flask import Flask
 
 
 app = Flask(__name__)
-app.config['DEBUG']=True
 
-@app.route('/home', methods=['GET'])
-def home():
-    return 'Adeus'
+@app.route("/")
+def Olá_Mundo():
+    return"<h1>Olá Mundo</h1>"
+
+@app.route("/ping")
+def ping():
+    return"<h1>você foi pingado</h1>"
+
+@app.route("/<name>")
+def diga_olá(name):
+    return f"<h1>Olá sou {name}!</h1>"
+
 
 
 app.run()
